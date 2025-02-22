180c94d0b6a77d694c389b0c0cc1dbad1ff54a3c
Arifia Kasastra R <arka.progammer@gmail.com>
2025-02-22 22:18:02 +0700

    Add Project model and migration for projects table

---
+++ app/Models/Project.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $id
+ * @property string $session_id
+ * @property string $name
+ * @property string $description
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Project extends Model
+{
+    use HasUuids;
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     * 
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     * 
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['session_id', 'name', 'description', 'created_at', 'updated_at'];
+}
