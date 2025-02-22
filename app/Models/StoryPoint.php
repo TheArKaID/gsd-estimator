1a6e022bc2b2a99e36a29c24c8ee925e55133dd8
Arifia Kasastra R <arka.progammer@gmail.com>
2025-02-23 02:52:54 +0700

    Add StoryPoint model and migration with relationship to Project

--- app/Models/StoryPoint.php
+++ app/Models/StoryPoint.php
@@ -4,18 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * @property string $id
- * @property string $session_id
+ * @property string $project_id
  * @property string $name
  * @property string $description
+ * @property int $value
  * @property string $created_at
  * @property string $updated_at
- * @property StoryPoint[] $storyPoints
+ * @property Project $project
  */
-class Project extends Model
+class StoryPoint extends Model
 {
     use HasUuids;
 
@@ -36,15 +37,15 @@ class Project extends Model
     /**
      * @var array
      */
-    protected $fillable = ['session_id', 'name', 'description', 'created_at', 'updated_at'];
+    protected $fillable = ['project_id', 'name', 'description', 'value', 'created_at', 'updated_at'];
 
     /**
-     * Get all of the storyPoints for the Project
+     * Get the project that owns the StoryPoint
      *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function storyPoints(): HasMany
+    public function project(): BelongsTo
     {
-        return $this->hasMany(StoryPoint::class);
+        return $this->belongsTo(Project::class);
     }
 }
