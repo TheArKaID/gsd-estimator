dfaf202a745b113512c9bd41aba8c66ecaa3c2f6
Arifia Kasastra R <arka.progammer@gmail.com>
2025-02-22 22:44:30 +0700

    Add route for project detail view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Livewire\Dashboard;
+use App\Livewire\DetailProject;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Dashboard::class);
+Route::get('/project/{id}', DetailProject::class);
