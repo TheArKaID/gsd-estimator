c4d9194511bf27b858c3a082c6fd94ff1b0e8a24
Arifia Kasastra R <arka.progammer@gmail.com>
2025-03-21 23:27:05 +0700

    Rename project_type to project_clarity and update migrations accordingly

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -12,7 +12,7 @@ use Illuminate\Database\Eloquent\Relations\HasManyThrough;
  * @property string $session_id
  * @property string $name
  * @property string $description
- * @property string $project_type
+ * @property string $project_clarity
  * @property string $team_size
  * @property string $velocity
  * @property string $created_at
@@ -42,7 +42,7 @@ class Project extends Model
     /**
      * @var array
      */
-    protected $fillable = ['session_id', 'name', 'description', 'project_type', 'team_size', 'velocity', 'created_at', 'updated_at'];
+    protected $fillable = ['session_id', 'name', 'description', 'project_clarity', 'team_size', 'velocity', 'created_at', 'updated_at'];
 
     /**
      * Get all of the storyPoints for the Project
