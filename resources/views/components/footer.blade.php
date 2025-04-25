7c9e342ba85e7f97c928209ece41ec4b45624c98
Arifia Kasastra R <arka.progammer@gmail.com>
2025-04-25 12:23:38 +0700

    Update footer copyright year and description for accuracy and clarity

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,9 @@
 <footer class="main-footer">
     <div class="footer-left">
-        <small>Copyright &copy; 2018 <div class="bullet"></div> Template By Muhamad Nauval Azhar</small>
+        <small>
+            Copyright &copy; {{ date('Y') }} <div class="bullet"></div>
+            Aplikasi GSD Estimation ini ditujukan sebagai salah satu instrumen penelitian Tesis Teknologi Informasi UGM.
+        </small>
     </div>
     <div class="footer-right">
         2.3.0
